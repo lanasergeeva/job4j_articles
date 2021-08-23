a895637363845ee6f8aa81263a209d6b82be1f51
PoymaLana <marikakopina@gmail.com>
2021-08-23 16:23:46 +0300

    2. Тестовое задание [#504790]

--- src/main/java/ru/job4j/articles/service/ArticleService.java
+++ src/main/java/ru/job4j/articles/service/ArticleService.java
@@ -4,6 +4,7 @@ import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
 import ru.job4j.articles.store.Store;
 
+
 public interface ArticleService {
     void generate(Store<Word> wordStore, int count, Store<Article> articleStore);
 }
